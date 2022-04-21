28fa0facb8e8ea76d7f4c42c802d822453f83bb0
Whenever <whenever.fansub@gmail.com>
2022-04-21 11:12:49 +0200

    delete ids

--- load.js
+++ load.js
@@ -7,8 +7,8 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 // Place your client and guild ids here
-const clientId = '664898677848145941';
-const guildId = '510724854920642560';
+const clientId = '';
+const guildId = '';
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
